dbcbbe96a71c6d72cfeb8dc69449bad9606f2990
robin <robin.schlenker10.10@gmail.com>
2024-04-18 11:38:16 -0400

    Setup with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { monthlyIncome } from 'affordable/src/finances'
+import { multipleYears } from 'affordable/src/future'
+
 export const metadata = {
-  title: "App Router",
-};
+  title: 'Can I afford',
+}
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  const test = multipleYears([monthlyIncome(2000)], 2022, 3, 0)
+  console.log(test)
+  return <h1>Can I afford</h1>
 }
