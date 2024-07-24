cf6e98b1b920264f32fae5c95ea506950996b508
Robin Schlenker <robin.schlenker@codecentric.de>
2024-07-24 08:32:14 +0200

    Remove unused manual addition tests

--- tests/app/BaseControlPanel.test.tsx
+++ tests/app/BaseControlPanel.test.tsx
@@ -8,33 +8,6 @@ import { setupStore } from '../../store/store'
 import { act } from 'react'
 jest.mock('../../app/actions/openAIAction', () => jest.fn())
 
-it.skip('should add a income factor', () => {
-  const { store } = renderWithProviders(<BaseControlPanel />)
-  fireEvent.click(screen.getByTestId('add-factor'))
-  fireEvent.click(screen.getByTestId('add-income'))
-
-  const incomeInput = screen.getByPlaceholderText('Value')
-  fireEvent.change(incomeInput, { target: { value: '5000' } })
-  const nameInput = screen.getByPlaceholderText('Name')
-  fireEvent.change(nameInput, { target: { value: 'anyName' } })
-  fireEvent.click(screen.getByText('save'))
-
-  const newFactor = store.getState().chart.factors[1].factor
-  const factorName = store.getState().chart.factors[1].name
-  expect(newFactor(0)).toBe(5000)
-  expect(factorName).toBe('anyName')
-  expect(screen.getByTestId('add-factor')).toBeVisible()
-})
-
-it.skip('should hide menu when showing form', () => {
-  renderWithProviders(<BaseControlPanel />)
-  expect(screen.queryByTestId('add-income')).not.toBeVisible()
-  fireEvent.click(screen.getByTestId('add-factor'))
-  fireEvent.click(screen.getByTestId('add-income'))
-  expect(screen.queryByTestId('add-income')).toBeNull()
-  expect(screen.queryByTestId('add-factor')).toBeNull()
-})
-
 it('should be able to delete factors', async () => {
   const store = setupStore()
   store.dispatch(addFactor({ factor: income(10), name: 'test-factor' }))
